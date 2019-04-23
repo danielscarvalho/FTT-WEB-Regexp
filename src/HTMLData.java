93cc67c385d7479ffb14f983ff857b3894f24c28
Daniel Carvalho <danielscarvalho@gmail.com>
2019-04-23 04:22:26 -0300

    First code...

---
+++ src/HTMLData.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.*;
+
+public class HTMLData {
+		 
+	 
+	public static void main(String[] args) throws Exception {
+
+		
+	    URL oracle = new URL("http://www.lan.esalq.usp.br/en/equipe/docentes");
+	    BufferedReader in = new BufferedReader(
+	    new InputStreamReader(oracle.openStream()));
+
+	    String inputLine, total="";
+	    StringBuffer sb = new StringBuffer();
+	    
+	    while ((inputLine = in.readLine()) != null) {
+	    	sb.append(inputLine);
+	    	total += inputLine;
+	        System.out.println(inputLine);
+	    }
+	        
+	    in.close();
+	    
+	    Pattern pattern1 = Pattern.compile("\\w+@\\w+.\\w+");
+	    Matcher matcher1 = pattern1.matcher(total);
+	    
+	    while (matcher1.find())
+	    	System.out.println(matcher1.group(0));
+	    
+    } //main
+
+} //HTMLData
+
