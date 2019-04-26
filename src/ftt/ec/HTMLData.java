fac4a5a37fbd2229a5f1fc9bcd06f9dade8cc20e
engineer <engineer@FTT-EC-VirtualBox>
2019-04-25 21:48:30 -0300

    fix var name

--- src/ftt/ec/HTMLData.java
+++ src/ftt/ec/HTMLData.java
@@ -13,10 +13,10 @@ public class HTMLData {
 		String url1 = "http://www.lan.esalq.usp.br/en/equipe/docentes";
 		String url2 = "http://127.0.0.1:37511/Faculty%20_%20Agri-food%20Industry,%20Food%20and%20Nutrition.html"; 
 		
-	    URL oracle = new URL(url2);
+	    URL urlConnect = new URL(url2);
 	    
 	    BufferedReader in = new BufferedReader(
-	    new InputStreamReader(oracle.openStream()));
+	    new InputStreamReader(urlConnect.openStream()));
 
 	    String inputLine;
 	    StringBuffer sb = new StringBuffer();
